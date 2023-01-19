a5bc97c28b7d16c9401a856c9c70a60ba5444b4e
HOME <codefordmr01@naver.com>
2023-01-19 20:17:44 +0900

    commit_finish

--- app/src/main/java/com/example/terid_off/EngnoteActivity.java
+++ app/src/main/java/com/example/terid_off/EngnoteActivity.java
@@ -1,6 +1,12 @@
 package com.example.terid_off;
 
+
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
@@ -14,6 +20,42 @@ public class EngnoteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_engnote);
+
+        Button tense = (Button)findViewById(R.id.tense);
+        tense.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), TenseActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button form = (Button)findViewById(R.id.form);
+        form.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), FormActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button partofspeech = (Button)findViewById(R.id.partofspeech);
+        partofspeech.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), PartofspeechActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button wordorder = (Button)findViewById(R.id.wordorder);
+        wordorder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), WordorderActivity.class);
+                startActivity(intent);
+            }
+        });
 
         // Toolbar 설정
         toolbar = findViewById(R.id.toolbar);
